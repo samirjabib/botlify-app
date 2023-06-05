f5434b95677f70c33e7ca7ea5b4e09b429128ae7
samirdev9669 <samirdev9669@gmail.com>
2023-06-05 11:40:54 -0500

    add test

--- lib/open-ai/tests/open-ai-stream.test.ts
+++ lib/open-ai/tests/open-ai-stream.test.ts
@@ -15,4 +15,56 @@ describe('OpenAIStreamPayload', () => {
           }),
         });
       });
+
+    afterEach(() => {
+    jest.restoreAllMocks();
+    });
+
+    async function consumeStream(stream : any) {
+        const decoder = new TextDecoder();
+        let data = '';
+      
+        const reader = stream.getReader();
+      
+        try {
+          while (true) {
+            const { done, value } = await reader.read();
+      
+            if (done) {
+              break;
+            }
+      
+            data += decoder.decode(value, { stream: !done });
+          }
+      
+          return data;
+        } finally {
+          reader.releaseLock();
+        }
+      }
+      
+
+
+    it('should fetch and process data correctly', async() => {
+        const payload: OpenAIStreamPayload = {
+            model: "model-name",
+            messages: [
+              { role: "user", content: "User message 1" },
+              { role: "system", content: "System message 1" },
+            ],
+            temperature: 0.8,
+            top_p: 0.9,
+            frequency_penalty: 0.6,
+            presence_penalty: 0.4,
+            max_tokens: 100,
+            stream: true,
+          };
+          
+          const result = await OpenAIStreamPayload(payload);
+
+          const response = await consumeStream(result);
+
+          expect(response).toBe('Mocked content');
+
+    })
 });
