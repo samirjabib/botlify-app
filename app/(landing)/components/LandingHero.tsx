19268e5b0198d84246830c4f19d94a10540478cc
samirdev9669 <samirdev9669@gmail.com>
2023-06-05 17:56:42 -0500

    add

--- app/(landing)/components/LandingHero.tsx
+++ app/(landing)/components/LandingHero.tsx
@@ -1,18 +1,18 @@
 const LandingHero = () => {
   return (
-    <div className="w-full   wrapper mt-44">
-      <div className="grid grid-cols-2">
+    <div className="w-full  wrapper mt-44">
+      <div className="grid grid-cols-1 md:grid-cols-2">
         <div className="">
-          <h1 className="text-white text-5xl font-bold mb-4 tracking-tighter max-w-[550px]">
+          <h1 className="text-white text-5xl font-bold mb-4 tracking-tighter max-w-[550px] text-center md:text-start">
             Impulsa tu negocio con bots inteligentes
           </h1>
-          <p className="text-lg text-white/80 tracking-tighter mb-4 font-medium max-w-[550px]">
+          <p className="text-lg text-white/80 tracking-tighter  font-medium max-w-[550px] text-center md:text-start">
             Aumenta la productividad y mejora tu servicio con nuestros bots
             inteligentes. Automatiza tareas, optimiza recursos y alcanza
             resultados excepcionales. Descubre cómo nuestra tecnología
             innovadora potencia tu negocio y lo lleva al siguiente nivel.
           </p>
-          <div className="flex w-[50%] gap-x-3 ">
+          <div className="flex flex-col md:flex-row w-full md:w-[50%] gap-x-3 gap-y-3 mt-16 justify-center md:justify-start">
             <button className="flex-1 text-lg bg-blue cursor-pointer text-white font-bold px-6 py-2 shadow-lg">
               Contactanos
             </button>
@@ -28,7 +28,7 @@ const LandingHero = () => {
             autoPlay
             loop
             muted
-            className="w-full"
+            className="w-full object-cover"
           />
         </div>
       </div>
