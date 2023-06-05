20a4184be70968aeace06f59ca8db6ba753ab224
samirdev9669 <samirdev9669@gmail.com>
2023-06-05 15:14:06 -0500

    add

--- app/(landing)/components/LandingHero.tsx
+++ app/(landing)/components/LandingHero.tsx
@@ -5,13 +5,13 @@ const LandingHero = () => {
     <div className="w-full flex justify-between  wrapper mt-32">
       <div className=" ">
         <h1 className="text-white text-5xl font-bold mb-4 tracking-tighter ">
-          Automatiza tus servicios con nuestros bots inteligentes
+          Impulsa tu negocio con bots inteligentes{" "}
         </h1>
-        <p className="text-lg text-white/80 tracking-tighter">
-          Descubre cómo nuestros bots de última generación pueden simplificar y
-          mejorar tus procesos, ahorrándote tiempo y recursos. Maximiza la
-          eficiencia de tus servicios y ofrece una experiencia excepcional a tus
-          clientes con nuestra tecnología avanzada.
+        <p className="text-lg text-white/80 tracking-tighter mb-4 font-medium">
+          Aumenta la productividad y mejora tu servicio con nuestros bots
+          inteligentes. Automatiza tareas, optimiza recursos y alcanza
+          resultados excepcionales. Descubre cómo nuestra tecnología innovadora
+          puede potenciar tu negocio y llevarlo al siguiente nivel.
         </p>
         <button className="bg-blue cursor-pointer text-white font-extrabold px-4 py-2">
           Contactanos
