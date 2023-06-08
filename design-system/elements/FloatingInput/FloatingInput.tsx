9d46bec72ea9e34ff4a2aba7eb88795336119682
samirdev9669 <samirdev9669@gmail.com>
2023-06-08 18:21:06 -0500

    add

--- design-system/elements/FloatingInput/FloatingInput.tsx
+++ design-system/elements/FloatingInput/FloatingInput.tsx
@@ -1,25 +1,32 @@
-import React, { HTMLProps } from "react";
+import { HTMLProps } from "react";
 import { getInputClasses, getLabelClasses } from "./getStyles";
 
 interface FloatingInputProps extends HTMLProps<HTMLInputElement> {
   type?: string;
-  className?: string;
 }
 
 const FloatingInput: React.FC<FloatingInputProps> = ({
   type = "text",
+  name,
+  id,
   className,
+  placeholder,
+  required,
   ...inputProps
 }) => {
   return (
     <div className="relative z-0 mb-6">
       <input
         type={type}
+        name={name}
+        id={id}
         className={getInputClasses(className)}
+        placeholder={placeholder}
+        required={required}
         {...inputProps}
       />
-      <label htmlFor={inputProps.id} className={getLabelClasses()}>
-        {inputProps.placeholder}
+      <label htmlFor={id} className={getLabelClasses()}>
+        {placeholder}
       </label>
     </div>
   );
