d30560f4f6d6ba2a137fe991b4c43e23ae9f8ea2
samirdev9669 <samirdev9669@gmail.com>
2023-06-08 17:10:31 -0500

    star making a form

--- components/LandingPage/LandingContact/LandingContact.tsx
+++ components/LandingPage/LandingContact/LandingContact.tsx
@@ -1,14 +1,31 @@
 const LandingContact = () => {
   return (
-    <div className="bg-darkGray w-full py-12 ">
-      <p className="text-white/60 text-sm text-center font-bold  mx-auto uppercase mb-2">
-        Contactanos para adquirir nuestros servicios
-      </p>
-      <h2 className="text-white text-4xl text-center font-bold mb-12 mx-auto max-w-3xl">
-        Descubre nuevas posibilidades y lleva tu servicio a un nivel superior.
-        ¡Contáctanos hoy mismo!
-      </h2>
-      <div className="w-full wrapper h-[600px]" />
+    <div className="bg-darkGray w-full py-12">
+      <div className="grid grid-cols-2 wrapper justify-center ">
+        {/* text-box first row */}
+        <div className="col-span-1 border-green-500 border-2">
+          <button>Contactanos</button>
+          <div>
+            <h2>Llena tu informacion y nos pondremos en contacto contigo</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              optio quia debitis? Ullam laborum dicta laudantium fugit
+              asperiores, rem eveniet?
+            </p>
+          </div>
+
+          <div></div>
+        </div>
+        {/* form second col */}
+        <div className="col-span-1  w-full border-yellow-500 border-2">
+          <form className="border-2 border-orange-500">
+            <div>
+              <label htmlFor="name">Nombre</label>
+              <input type="text" name="name" id="name" />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
