9ca615ff2b855dc78180085e74243f1d8e10f92f
samirdev9669 <samirdev9669@gmail.com>
2023-06-08 12:23:34 -0500

    add

--- components/LandingPage/LandingBenefits/LandingBenefits.tsx
+++ components/LandingPage/LandingBenefits/LandingBenefits.tsx
@@ -3,12 +3,39 @@ const LandingBenefits = () => {
     <div className="bg-darkGray text-white wrapper">
       <div className="w-full flex flex-col items-center sm:flex-row py-24 border-2-border-red-500">
         <div className="w-full sm:w-[50%]">
-          <h3>Ahorra tiempo y recursos</h3>
-          <p>Automatiza tus procesos y reduce la carga de trabajo manual.</p>
-          <ul>
-            <li>Aumenta la eficiencia operativa.</li>
-            <li>Optimiza la asignación de recursos.</li>
-            <li>Minimiza errores y retrabajos.</li>
+          <h3 className=" font-semibold text-2xl mb-4 tracking-tighter">
+            Optimiza tus operaciones y ahorra tiempo
+          </h3>
+          <p className="text-white/80 text-base tracking-tighter mb-4  max-w-lg pr-5">
+            Descubre cómo puedes mejorar la eficiencia y reducir la carga de
+            trabajo manual mediante la automatización de tus procesos. Con la
+            automatización, podrás:
+          </p>
+          <ul className="text-white/80 list-disc pl-4 text-base tracking-tighter flex flex-col gap-y-2 max-w-lg pr-5">
+            <li>
+              <span className="text-blue">Aumenta</span> la productividad y
+              eficiencia operativa al eliminar tareas repetitivas y tediosas.
+            </li>
+            <li>
+              <span className="text-blue">Optimiza</span> la asignación
+              inteligente de recursos al identificar y eliminar ineficiencias en
+              la distribución de tareas.
+            </li>
+            <li>
+              <span className="text-blue">Minimiza</span> errores y evita
+              retrabajos innecesarios al implementar flujos de trabajo
+              automatizados y controles de calidad.
+            </li>
+            <li>
+              <span className="text-blue">Gana</span> tiempo para centrarte en
+              tareas estratégicas y de mayor valor que impulsen el crecimiento
+              de tu negocio.
+            </li>
+            <li>
+              <span className="text-blue">Mejora</span> la precisión y
+              consistencia de tus procesos al reducir la dependencia de acciones
+              manuales propensas a errores.
+            </li>
           </ul>
         </div>
         <div className="w-[50%] flex justify-center">
