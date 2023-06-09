899112d0698aa92e3d19ddb8a056472756df3d97
samirdev9669 <samirdev9669@gmail.com>
2023-06-09 12:45:16 -0500

    test area

--- components/LandingPage/LandingContact/LandingContact.tsx
+++ components/LandingPage/LandingContact/LandingContact.tsx
@@ -5,18 +5,35 @@ const LandingContact = () => {
     <div className="bg-darkGray w-full py-12">
       <div className="grid grid-cols-2 wrapper justify-center space-x-12">
         {/* text-box first row */}
-        <div className="col-span-1 border-green-500 border-2">
+        <div className="col-span-1 border-green-500 border-2 px-8">
           <button>Contactanos</button>
           <div>
-            <h2>Llena tu informacion y nos pondremos en contacto contigo</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-              optio quia debitis? Ullam laborum dicta laudantium fugit
-              asperiores, rem eveniet?
+            <h2 className="text-white">
+              Amplifica tu éxito comercial: Potencia tus ventas con nuestra
+              solución avanzada
+            </h2>
+            <p className="text-white/80">
+              Automatización potente y mayor satisfacción del cliente con
+              nuestro chatbot inteligente.
             </p>
           </div>
 
-          <div></div>
+          <div>
+            <p className="text-white/80 text-start text-sm ">
+              "El bot para mi tienda de comercio electrónico ha sido
+              transformador. Mejoró la interacción con los clientes, impulsó las
+              ventas y automatizó tareas. ¡Lo recomiendo ampliamente para
+              mejorar cualquier negocio en línea!"
+            </p>
+            <div className="flex flex-row gap-x-2   justify-start">
+              <div className="h-8 w-8 bg-yellow-500 rounded-full" />
+              <h3 className="text-white/80">Samir Jabib</h3>
+              <div className="h-3  border border-white/60" />
+              <p className=" text-white/60 text-sm">
+                Propietario de E-commerce
+              </p>
+            </div>
+          </div>
         </div>
         {/* form second col */}
         <div className="col-span-1  w-full border-yellow-500 border-2 flex justify-center">
