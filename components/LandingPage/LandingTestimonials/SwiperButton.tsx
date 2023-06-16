eced4430e7146190451d3e4591ce5d3eedc6ea85
samirdev9669 <samirdev9669@gmail.com>
2023-06-16 15:38:33 -0500

    make swiper button

---
+++ components/LandingPage/LandingTestimonials/SwiperButton.tsx
@@ -0,0 +1,22 @@
+import clsx from "clsx";
+import { HTMLAttributes, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+
+const baseClass =
+  "text-white/60 text-base transition-all hover:text-white cursor-pointer";
+
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+}
+
+function SwiperButton({ children, className, ...props }: ButtonProps) {
+  const classNames = twMerge(className);
+
+  return (
+    <button className={clsx(baseClass, classNames)} {...props}>
+      {children}
+    </button>
+  );
+}
+
+export default SwiperButton;
