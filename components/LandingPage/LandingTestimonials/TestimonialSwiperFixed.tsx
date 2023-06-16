658b73e3f9ff94db6a2f18e3b9d673b3817f54c3
samirdev9669 <samirdev9669@gmail.com>
2023-06-16 17:33:41 -0500

    surrender swiper

--- components/LandingPage/LandingTestimonials/TestimonialSwiperFixed.tsx
+++ components/LandingPage/LandingTestimonials/TestimonialSwiperFixed.tsx
@@ -1,16 +1,11 @@
 "use client";
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 
-// Import Swiper styles
-import "swiper/swiper-bundle.min.css";
-import "swiper/css/pagination";
 
 import { MoveLeft, MoveRight } from "lucide-react";
 import TestimonialItem from "./TestimonialItem";
 import SwiperButton from "./SwiperButton";
 
 export const TestimonialSwiper = () => {
-  //useSwiper is bugged, so we wait for update to fix it in swiperV9
   const handleSlideNext = () => {};
 
   const handleSlidePrev = () => {
@@ -18,17 +13,11 @@ export const TestimonialSwiper = () => {
   };
 
   return (
-    <Swiper
-      className="w-full  flex flex-col items-center max-w-4xl"
-      slidesPerView={1}
-      grabCursor={false}
-      pagination={{ clickable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
+    <div className="w-full  flex flex-col items-center max-w-4xl">
       {Array.from({ length: 3 }).map((_, i) => (
-        <SwiperSlide className=" w-full " key={i}>
+        <div className=" w-full " key={i}>
           <TestimonialItem />
-        </SwiperSlide>
+        </div>
       ))}
       <div className="flex flex-row gap-x-4 justify-center items-center mt-8">
         <SwiperButton onClick={handleSlideNext}>
@@ -38,7 +27,7 @@ export const TestimonialSwiper = () => {
           <MoveRight className="text-white/60 text-base transition-all hover:text-white cursor-pointer" />
         </SwiperButton>
       </div>
-    </Swiper>
+    </div>
   );
 };
 
