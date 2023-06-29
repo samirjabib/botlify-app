0a2cefe9d2559adbe07d63f2c4c39284aec94c2c
samirdev9669 <samirdev9669@gmail.com>
2023-06-29 15:14:33 -0500

    add

---
+++ components/blog/blog-card.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FC } from "react";
+
+type Author = {
+  name: string;
+  image: string;
+};
+
+type BlogCardProps = {
+  image: string;
+  title: string;
+  description: string;
+  date: string;
+  author: Author;
+  slug: string;
+};
+
+const BlogCard: FC<BlogCardProps> = ({
+  author,
+  date,
+  description,
+  image,
+  slug,
+  title,
+}) => {
+    
+  const formatDate = (date: string) => {
+    const newDate = new Date(date);
+    const formattedDate = `${newDate.getDate()}-${
+      newDate.getMonth() + 1
+    }-${newDate.getFullYear()}`;
+    return formattedDate;
+  };
+
+  return (
+    <Link href={`/blog/${slug}`}>
+      <div className="bg-white p-6 rounded-lg shadow-md">
+        <div className="h-48 w-full mb-6 overflow-hidden">
+          <Image
+            width={200}
+            height={200}
+            src={image}
+            alt={title}
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <div>
+          <h2 className="text-2xl font-bold mb-2">{title}</h2>
+          <p className="text-gray-500 mb-4">{description.slice(0, 200)}...</p>
+          <div className="flex justify-between">
+            <p className="text-gray-500">By {author.name}</p>
+            {/* <p className="text-gray-500"">{formatDate(date)}</p> */}
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default BlogCard;
--- components/blog/blog-category-card.tsx
+++ components/blog/blog-category-card.tsx
@@ -2,60 +2,29 @@ import Image from "next/image";
 import Link from "next/link";
 import { FC } from "react";
 
-type Author = {
+interface BlogCategoryCardProps {
+  image: string;
   name: string;
-  image: string;
-};
-
-type BlogCardProps = {
-  image: string;
-  title: string;
-  description: string;
-  date: string;
-  author: Author;
   slug: string;
-};
+  description: string;
+}
 
-const BlogCard: FC<BlogCardProps> = ({
-  author,
-  date,
-  description,
-  image,
-  slug,
-  title,
-}) => {
-    
-  const formatDate = (date: string) => {
-    const newDate = new Date(date);
-    const formattedDate = `${newDate.getDate()}-${
-      newDate.getMonth() + 1
-    }-${newDate.getFullYear()}`;
-    return formattedDate;
-  };
+const BlogCategoryCard: FC<BlogCategoryCardProps> = (props) => {
+  const { image, name, slug, description } = props;
 
   return (
-    <Link href={`/blog/${slug}`}>
-      <div className="bg-white p-6 rounded-lg shadow-md">
-        <div className="h-48 w-full mb-6 overflow-hidden">
-          <Image
-            width={200}
-            height={200}
-            src={image}
-            alt={title}
-            className="h-full w-full object-cover"
-          />
-        </div>
-        <div>
-          <h2 className="text-2xl font-bold mb-2">{title}</h2>
-          <p className="text-gray-500 mb-4">{description.slice(0, 200)}...</p>
-          <div className="flex justify-between">
-            <p className="text-gray-500">By {author.name}</p>
-            {/* <p className="text-gray-500"">{formatDate(date)}</p> */}
-          </div>
-        </div>
-      </div>
-    </Link>
+    <div className="w-full overflow-hidden bg-white p-6 rounded-lg shadow-md">
+      <Image
+        width={200}
+        height={200}
+        className="h-48 w-full object-cover rounded-lg mb-6 hover:scale-125 transition-all duration-500"
+        src={image}
+        alt={name}
+      />
+      <h3 className="text-xl font-bold mb-2">{name}</h3>
+      <p className="text-gray-500">{description.slice(0, 100)}...</p>
+    </div>
   );
 };
 
-export default BlogCard;
+export default BlogCategoryCard;
